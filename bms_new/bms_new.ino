07ef0ced8b435330f3cf9d573cc91ea429092254
Jackiempty <candypaper0123@gmail.com>
2024-05-14 20:01:27 +0800

    Rewrite charge, discharge and read voltage
    Other things are also included such as starttowork, moniV, and charge_balance
    are set to "to be eliminated" since those are from the old one and should be
    gotten rid of.

--- bms_new/bms_new.ino
+++ bms_new/bms_new.ino
@@ -14,13 +14,17 @@
 /**************** Local Function Declaration *******************/
 /****** Stock ******/
 void check_error(int error);
+void print_cells(uint8_t datalog_en);
 /****** Custom ******/
+void read_voltage();
 void discharge();
 void stop_discharge();
 void calculate();
+// ************ belows are waited to be eliminated **************
 void starttowork();
-void moniV(); // called by starttowork
-void battery_charge_balance(); // called by starttowork
+void moniV();                   // called by starttowork
+void battery_charge_balance();  // called by starttowork
+void temp_detect();             // called by calculate
 
 /*******************************************************************
   Setup Variables
@@ -29,7 +33,7 @@ void battery_charge_balance(); // called by starttowork
 /****** Stock ******/
 const uint8_t TOTAL_IC = 10;  //!< Number of ICs in the daisy chain
 
-cell_asic BMS_IC[TOTAL_IC]; //!< Global Battery Variable
+cell_asic BMS_IC[TOTAL_IC];  //!< Global Battery Variable
 
 /****** Custom *****/
 double vmin[TOTAL_IC];
@@ -51,7 +55,6 @@ void setup() {
 }
 
 void loop() {
-
   if (shutdown_status) {
     digitalWrite(BMS_FAULT_PIN, HIGH);
   } else {
@@ -65,65 +68,100 @@ void loop() {
 
 /**************** Local Function Implementation ****************/
 /****** Stock ******/
-void check_error(int error)
-{
-  if (error == -1)
-  {
+void check_error(int error) {
+  if (error == -1) {
     Serial.println(F("A PEC error was detected in the received data"));
   }
 }
 
+void print_cells(uint8_t datalog_en) {
+  for (int current_ic = 0; current_ic < TOTAL_IC; current_ic++) {
+    if (datalog_en == 0) {
+      Serial.print(" IC ");
+      Serial.print(current_ic + 1, DEC);
+      Serial.print(": ");
+      for (int i = 0; i < BMS_IC[0].ic_reg.cell_channels; i++) {
+        Serial.print(" C");
+        Serial.print(i + 1, DEC);
+        Serial.print(":");
+        Serial.print(BMS_IC[current_ic].cells.c_codes[i] * 0.0001, 4);
+        Serial.print(",");
+      }
+      Serial.println();
+    } else {
+      Serial.print(" Cells :");
+      for (int i = 0; i < BMS_IC[0].ic_reg.cell_channels; i++) {
+        Serial.print(BMS_IC[current_ic].cells.c_codes[i] * 0.0001, 4);
+        Serial.print(",");
+      }
+    }
+  }
+  Serial.println("\n");
+}
 /****** Custom ******/
+void read_voltage() {
+  wakeup_sleep(TOTAL_IC);
+  LTC6811_adcv(ADC_CONVERSION_MODE, ADC_DCP, CELL_CH_TO_CONVERT);
+  conv_time = LTC6811_pollAdc();
+  print_conv_time(conv_time);
+  wakeup_idle(TOTAL_IC);
+  error = LTC6811_rdcv(SEL_ALL_REG, TOTAL_IC,
+                       BMS_IC);  // Set to read back all cell voltage registers
+  check_error(error);
+  print_cells(DATALOG_DISABLED);
+}
+
 void discharge() {
+  int8_t error = 0;
+  uint32_t conv_time = 0;
+
   wakeup_sleep(TOTAL_IC);
   LTC6811_adcv(ADC_CONVERSION_MODE, ADC_DCP, CELL_CH_TO_CONVERT);
   conv_time = LTC6811_pollAdc();
   error = LTC6811_rdcfg(TOTAL_IC, BMS_IC);
   check_error(error);  // Check error to enable the function
   for (int i = 0; i < TOTAL_IC; i++) {
+    // ******** need to change the usage of consvmin and vmin ********
     consvmin[i] = vmin[i];  // Set up a costant Vminimum in case of the
                             // minumum become lower and lower
   }
-  for (int current_ic = 0; current_ic < TOTAL_IC; current_ic++) {
-    for (int i = 0; i < BMS_IC[0].ic_reg.cell_channels; i++) {
-      if (BMS_IC[current_ic].cells.c_codes[i] * 0.0001 > consvmin[current_ic]) {
-        wakeup_sleep(TOTAL_IC);
-        LTC6811_set_custom_discharge(i + 1, TOTAL_IC - (current_ic + 1),
-                                     TOTAL_IC, BMS_IC);
-        LTC6811_wrcfg(TOTAL_IC, BMS_IC);
-        discharge_stat[current_ic][i] = true;
-      }
+
+  for (int i = 0; i < BMS_IC[0].ic_reg.cell_channels; i++) {
+    if (BMS_IC[current_ic].cells.c_codes[i] * 0.0001 > consvmin[current_ic]) {
+      wakeup_sleep(TOTAL_IC);
+      LTC6811_set_discharge(i + 1, TOTAL_IC, BMS_IC);
+      LTC6811_wrcfg(TOTAL_IC, BMS_IC);
     }
   }
-  start_discharge = true;
+
   Serial.println(F("start discharge"));
-  return;
 }
 
 void stop_discharge() {
+  int8_t error = 0;
+  uint32_t conv_time = 0;
+
   Serial.println(F("Stop all discharge !!"));
   wakeup_sleep(TOTAL_IC);
-  RDS = 0;
   LTC6811_adcv(ADC_CONVERSION_MODE, ADC_DCP, CELL_CH_TO_CONVERT);
   conv_time = LTC6811_pollAdc();
   error = LTC6811_rdcv(SEL_ALL_REG, TOTAL_IC, BMS_IC);
   check_error(error);
 
-  start_discharge = false;
-  start_balance = false;
   Serial.println(F("Stop balance !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"));
 
-  for (int current_ic = 0; current_ic < TOTAL_IC; current_ic++) {
-    for (int i = 0; i < BMS_IC[0].ic_reg.cell_channels; i++) {
-      wakeup_sleep(TOTAL_IC);
-      LTC6811_clear_custom2_discharge(i + 1, current_ic, TOTAL_IC, BMS_IC);
-      LTC6811_wrcfg(TOTAL_IC, BMS_IC);
-    }
+  // ******** make the set_discharge function back to stock one *********
+  for (int i = 0; i < BMS_IC[0].ic_reg.cell_channels; i++) {
+    wakeup_sleep(TOTAL_IC);
+    LTC6811_clear_discharge(i + 1, TOTAL_IC, BMS_IC);
+    LTC6811_wrcfg(TOTAL_IC, BMS_IC);
   }
-  return;
 }
 
 void calculate() {  // calculate minimal and maxium (cmd 92)
+  int8_t error = 0;
+  uint32_t conv_time = 0;
+
   wakeup_sleep(TOTAL_IC);
   LTC6811_adcv(ADC_CONVERSION_MODE, ADC_DCP, CELL_CH_TO_CONVERT);
   error = LTC6811_rdcv(SEL_ALL_REG, TOTAL_IC,
@@ -142,9 +180,9 @@ void calculate() {  // calculate minimal and maxium (cmd 92)
     }
   }
   temp_detect();
-
-  return;
 }
+
+// ************ belows are waited to be eliminated **************
 
 void starttowork() {
   Serial.println("StartToWork");
@@ -205,7 +243,7 @@ void moniV()  // Every loop will Check out the Discharging cell is lower than
   }
 
   return;
-} // called by starttowork
+}  // called by starttowork
 
 void battery_charge_balance() {
   uint32_t conv_time = 0;
@@ -245,4 +283,66 @@ void battery_charge_balance() {
   }
 
   return;
-} // called by starttowork
+}  // called by starttowork
+
+void temp_detect() {
+  uint32_t conv_time = 0;
+  int8_t error = 0;
+  wakeup_sleep(TOTAL_IC);
+  LTC6811_adax(ADC_CONVERSION_MODE, AUX_CH_TO_CONVERT);
+  conv_time = LTC6811_pollAdc();
+  error =
+      LTC6811_rdaux(0, TOTAL_IC, BMS_IC);  // Set to read back all aux registers
+  check_error(error);
+  delay(100);
+
+  const float THSourceVoltage = 3.0;
+  const int THRES = 10000;
+  const float RT0 = 10000;  // 常溫 25度時的 NTC 電阻值
+  const float RT1 = 32650;  // 0度時的 NTC 電阻值
+  const float RT2 = 588.6;  // 105度時的 NTC 電阻值
+  const float T0 = 298.15;  // 常溫 25度時的 Kelvin 值
+  const float T1 = 273.15;  // 0度時的 Kelvin 值
+  const float T2 = 378.15;  // 105度時的 Kelvin 值
+
+  for (int current_ic = 0; current_ic < TOTAL_IC;
+       current_ic++)  // 2 5 not work  //0 1 3 4 7 detect
+  {
+    for (int i = 0; i < 12; i++) {
+      Serial.print(BMS_IC[current_ic].aux.a_codes[i]);
+      Serial.print(", ");
+    }
+
+    Serial.println("###############");
+    Serial.println();
+  }
+  float tf = 0;
+  for (int current_ic = 0; current_ic < TOTAL_IC;
+       current_ic++)  // 2 5 not work  //0 1 3 4 7 detect
+  {
+    for (int i = 0; i < 8;
+         i++) {  // GPIO->V   10V/(5-V)=R=10*exp(3984*(1/T-1/298.15)) Rt = R
+                 // *EXP(B*(1/T1-1/T2))
+      if (BMS_IC[current_ic].aux.a_codes[i] != 0) {
+        float value;
+        float VoltageOut;
+        float ROut;
+        float beta;
+        float Rx;
+        float KelvinValue;
+
+        beta = (log(RT1 / RT2)) / ((1 / T1) - (1 / T2));
+        Rx = RT0 * exp(-beta / T0);
+
+        VoltageOut = BMS_IC[current_ic].aux.a_codes[i] * 0.0001;
+
+        ROut = THRES * VoltageOut /
+               (THSourceVoltage - VoltageOut);  // 目前 NTC 電阻值
+        KelvinValue = (beta / log(ROut / Rx));
+
+        BMS_IC[current_ic].aux.a_codes[i] =
+            KelvinValue - 273.15;  // Kelvin 轉 溫度C
+      }
+    }
+  }
+}
