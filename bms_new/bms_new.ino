dc70cbc0357fd4e2b4b8b618066ec92f80a4ef4a
Jackiempty <candypaper0123@gmail.com>
2024-05-17 16:36:44 +0800

    Add read_volgate() to loop and formatted

--- bms_new/bms_new.ino
+++ bms_new/bms_new.ino
@@ -37,53 +37,78 @@ void temp_detect();             // called by calculate
   Setup Variables
   The following variables can be modified to configure the software.
 ********************************************************************/
-/****** Stock ******/
+/****************** Stock *******************/
 const uint8_t TOTAL_IC = 10;  //!< Number of ICs in the daisy chain
 
-//ADC Command Configurations. See LTC681x.h for options.
-const uint8_t ADC_OPT = ADC_OPT_DISABLED; //!< ADC Mode option bit
-const uint8_t ADC_CONVERSION_MODE = MD_7KHZ_3KHZ; //!< ADC Mode
-const uint8_t ADC_DCP = DCP_ENABLED; //!< Discharge Permitted 
-const uint8_t CELL_CH_TO_CONVERT = CELL_CH_ALL; //!< Channel Selection for ADC conversion
-const uint8_t AUX_CH_TO_CONVERT = AUX_CH_ALL; //!< Channel Selection for ADC conversion
-const uint8_t STAT_CH_TO_CONVERT = STAT_CH_ALL; //!< Channel Selection for ADC conversion
-const uint8_t SEL_ALL_REG = REG_ALL; //!< Register Selection 
-const uint8_t SEL_REG_A = REG_1; //!< Register Selection 
-const uint8_t SEL_REG_B = REG_2; //!< Register Selection 
+// ADC Command Configurations. See LTC681x.h for options.
+const uint8_t ADC_OPT = ADC_OPT_DISABLED;          //!< ADC Mode option bit
+const uint8_t ADC_CONVERSION_MODE = MD_7KHZ_3KHZ;  //!< ADC Mode
+const uint8_t ADC_DCP = DCP_ENABLED;               //!< Discharge Permitted
+const uint8_t CELL_CH_TO_CONVERT =
+    CELL_CH_ALL;  //!< Channel Selection for ADC conversion
+const uint8_t AUX_CH_TO_CONVERT =
+    AUX_CH_ALL;  //!< Channel Selection for ADC conversion
+const uint8_t STAT_CH_TO_CONVERT =
+    STAT_CH_ALL;                      //!< Channel Selection for ADC conversion
+const uint8_t SEL_ALL_REG = REG_ALL;  //!< Register Selection
+const uint8_t SEL_REG_A = REG_1;      //!< Register Selection
+const uint8_t SEL_REG_B = REG_2;      //!< Register Selection
 
-const uint16_t MEASUREMENT_LOOP_TIME = 500; //!< Loop Time in milliseconds(ms)
+const uint16_t MEASUREMENT_LOOP_TIME = 500;  //!< Loop Time in milliseconds(ms)
 
-//Under Voltage and Over Voltage Thresholds
-const uint16_t OV_THRESHOLD = 41000; //!< Over voltage threshold ADC Code. LSB = 0.0001 ---(4.1V)
-const uint16_t UV_THRESHOLD = 30000; //!< Under voltage threshold ADC Code. LSB = 0.0001 ---(3V)
+// Under Voltage and Over Voltage Thresholds
+const uint16_t OV_THRESHOLD =
+    41000;  //!< Over voltage threshold ADC Code. LSB = 0.0001 ---(4.1V)
+const uint16_t UV_THRESHOLD =
+    30000;  //!< Under voltage threshold ADC Code. LSB = 0.0001 ---(3V)
 
-//Loop Measurement Setup. These Variables are ENABLED or DISABLED. Remember ALL CAPS
-const uint8_t WRITE_CONFIG = DISABLED;  //!< This is to ENABLED or DISABLED writing into to configuration registers in a continuous loop
-const uint8_t READ_CONFIG = DISABLED; //!< This is to ENABLED or DISABLED reading the configuration registers in a continuous loop
-const uint8_t MEASURE_CELL = ENABLED; //!< This is to ENABLED or DISABLED measuring the cell voltages in a continuous loop
-const uint8_t MEASURE_AUX = DISABLED; //!< This is to ENABLED or DISABLED reading the auxiliary registers in a continuous loop
-const uint8_t MEASURE_STAT = DISABLED; //!< This is to ENABLED or DISABLED reading the status registers in a continuous loop
-const uint8_t PRINT_PEC = DISABLED; //!< This is to ENABLED or DISABLED printing the PEC Error Count in a continuous loop
+// Loop Measurement Setup. These Variables are ENABLED or DISABLED. Remember ALL
+// CAPS
+const uint8_t WRITE_CONFIG =
+    DISABLED;  //!< This is to ENABLED or DISABLED writing into to configuration
+               //!< registers in a continuous loop
+const uint8_t READ_CONFIG =
+    DISABLED;  //!< This is to ENABLED or DISABLED reading the configuration
+               //!< registers in a continuous loop
+const uint8_t MEASURE_CELL =
+    ENABLED;  //!< This is to ENABLED or DISABLED measuring the cell voltages in
+              //!< a continuous loop
+const uint8_t MEASURE_AUX =
+    DISABLED;  //!< This is to ENABLED or DISABLED reading the auxiliary
+               //!< registers in a continuous loop
+const uint8_t MEASURE_STAT =
+    DISABLED;  //!< This is to ENABLED or DISABLED reading the status registers
+               //!< in a continuous loop
+const uint8_t PRINT_PEC =
+    DISABLED;  //!< This is to ENABLED or DISABLED printing the PEC Error Count
+               //!< in a continuous loop
 
 cell_asic BMS_IC[TOTAL_IC];  //!< Global Battery Variable
 
-/****** Custom *****/
+/****************** Custom ******************/
 double vmin[TOTAL_IC];
 double vmax[TOTAL_IC];
 double consvmin[TOTAL_IC];
 
 /*********************************************************
- Set the configuration bits. 
- Refer to the Configuration Register Group from data sheet. 
+ Set the configuration bits.
+ Refer to the Configuration Register Group from data sheet.
 **********************************************************/
-bool REFON = true; //!< Reference Powered Up Bit
-bool ADCOPT = false; //!< ADC Mode option bit
-bool GPIOBITS_A[5] = {false,false,true,true,true}; //!< GPIO Pin Control // Gpio 1,2,3,4,5
-uint16_t UV=UV_THRESHOLD; //!< Under-voltage Comparison Voltage
-uint16_t OV=OV_THRESHOLD; //!< Over-voltage Comparison Voltage
-bool DCCBITS_A[12] = {false,false,false,false,false,false,false,false,false,false,false,false}; //!< Discharge cell switch //Dcc 1,2,3,4,5,6,7,8,9,10,11,12
-bool DCTOBITS[4] = {true, false, true, false}; //!< Discharge time value // Dcto 0,1,2,3 // Programed for 4 min 
-/*Ensure that Dcto bits are set according to the required discharge time. Refer to the data sheet */
+bool REFON = true;    //!< Reference Powered Up Bit
+bool ADCOPT = false;  //!< ADC Mode option bit
+bool GPIOBITS_A[5] = {false, false, true, true,
+                      true};  //!< GPIO Pin Control // Gpio 1,2,3,4,5
+uint16_t UV = UV_THRESHOLD;   //!< Under-voltage Comparison Voltage
+uint16_t OV = OV_THRESHOLD;   //!< Over-voltage Comparison Voltage
+bool DCCBITS_A[12] = {
+    false, false, false, false, false, false,
+    false, false, false, false, false, false};  //!< Discharge cell switch //Dcc
+                                                //!< 1,2,3,4,5,6,7,8,9,10,11,12
+bool DCTOBITS[4] = {
+    true, false, true,
+    false};  //!< Discharge time value // Dcto 0,1,2,3 // Programed for 4 min
+/*Ensure that Dcto bits are set according to the required discharge time. Refer
+ * to the data sheet */
 
 void setup() {
   Serial.begin(115200);
@@ -104,6 +129,8 @@ void loop() {
   quikeval_SPI_connect();
   spi_enable(
       SPI_CLOCK_DIV16);  // This will set the Linduino to have a 1MHz Clock
+
+  read_voltage();
 }
 
 /**************** Local Function Implementation ****************/
@@ -140,10 +167,10 @@ void print_cells(uint8_t datalog_en) {
 }
 
 void print_conv_time(uint32_t conv_time) {
-  uint16_t m_factor=1000;  // to print in ms
+  uint16_t m_factor = 1000;  // to print in ms
 
   Serial.print(F("Conversion completed in:"));
-  Serial.print(((float)conv_time/m_factor), 1);
+  Serial.print(((float)conv_time / m_factor), 1);
   Serial.println(F("ms \n"));
 }
 /****** Custom ******/
@@ -200,11 +227,10 @@ void stop_discharge() {
   Serial.println(F("Stop balance !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"));
 
   // ******** make the set_discharge function back to stock one *********
-  
+
   wakeup_sleep(TOTAL_IC);
   LTC6811_clear_discharge(TOTAL_IC, BMS_IC);
   LTC6811_wrcfg(TOTAL_IC, BMS_IC);
-  
 }
 
 void calculate() {  // calculate minimal and maxium (cmd 92)
